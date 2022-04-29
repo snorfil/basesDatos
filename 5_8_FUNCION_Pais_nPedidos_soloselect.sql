f9efb93983d9d7bfc83f9f6e218fbd8fcf7169a7
Edwin leandro <Snorfil@hotmail.com>
2022-04-29 19:34:59 +0200

    restructuracion de ficheros

--- 5_8_FUNCION_Pais_nPedidos_soloselect.sql
+++
@@ -1,6 +0,0 @@
-SELECT VENTA.paises.nombrepais, 
-	Count(VENTA.pedidoscabe.idpedido) AS CuentaDeidpedido
-FROM VENTA.paises INNER JOIN VENTA.clientes ON VENTA.paises.idpais = VENTA.clientes.idpais 
-INNER JOIN VENTA.pedidoscabe ON VENTA.clientes.idcliente = VENTA.pedidoscabe.idcliente
-GROUP BY VENTA.paises.nombrepais
-HAVING Count(VENTA.pedidoscabe.idpedido)>1;
--- 5_9_select.sql
+++
@@ -1,4 +0,0 @@
-SELECT top 1 VENTA.clientes.idcliente, VENTA.clientes.nombrecia
-FROM VENTA.clientes, VENTA.pedidoscabe
-WHERE (((Year([fechapedido]))=2013))
-ORDER BY VENTA.pedidoscabe.fechapedido DESC;
